f386df6f969e6abaaaec766e8e677cd4d797b946
Hai Nguyen <hai.nguyen@salesforce.com>
2016-02-15 21:05:19 -0500

    [Mon Feb 15 21:05:19 EST 2016] dragging works

--- src/components/EditorRight/Slot/Block/block.js
+++ src/components/EditorRight/Slot/Block/block.js
@@ -1,37 +1,39 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { DragSource, DropTarget } from 'react-dnd'
 
 import { dragBlock } from '../../../../Actions/actions'
 
 import './block.less'
 
+const blockSource = {
+	beginDrag(props) {
+
+		console.log('yo', props.block.id)
+
+		return {
+			id: props.block.id
+		}
+	}
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => {
 	return {
 		onDragStart: () => {
-			// console.log('yo', ownProps);
-			// dispatch(addBlock())
-
 			dispatch(dragBlock(ownProps.slotId, ownProps.block.id))
 		}
 	}
 }
 
+
 @connect(undefined, mapDispatchToProps)
+@DragSource('BLOCK', blockSource, (connect, monitor) => ({
+	connectDragSource: connect.dragSource(),
+  	isDragging: monitor.isDragging()
+}))
 export default class Block extends Component {
-
-
-
-
-	// const onDragStart = () => {
-	// 	console.log('context', this.context);
-	// }
-
-	// console.count('block render');
-
-	
 	render() {
-		const { block, slotId, onDragStart } = this.props;
+		const { block, slotId, onDragStart, connectDragSource } = this.props;
 	
 		let styles = { display: 'block' }
 
@@ -39,14 +41,12 @@ export default class Block extends Component {
 		let blockInnards = <div className="innards" style={ { background: 'blue', height: 50 } }>NNNNNNNNNNNNNNNNNNNNNNNN</div>
 
 		if (block.name == 'dragging') {		
-
-
 			blockInnards = <p className="innards" style={ { height: 75 } }>draggingdraggingdraggingdraggingdraggingdraggingdragging</p>
 		}
 
 
-		return (
-			<div className="block" onDragStart={onDragStart} draggable="true" style={styles}>
+		return connectDragSource(
+			<div className="block" style={styles}>
 				<span className="name">{block.name}{block.id}</span>
 				{blockInnards}
 			</div>
@@ -55,99 +55,3 @@ export default class Block extends Component {
 
 }
 
-// const mapStateToProps = (state) => {
-// 	return {
-// 		block: state.slot
-// 	}
-// }
-
-			// <button style={{margin: 'auto', display: 'block'}} onClick={onClick}>clicky</button>
-
-
-
-
-
-// export default Block
-
-// export default connect(undefined, mapDispatchToProps)(Block)
-
-
-
-
-/*
-
-module.exports = React.createClass({
-	dragStart: function (e) {
-		var blockInfo = {
-			id: this.props.data.id,
-			parentSlotId: this.props.parentSlotId
-		}
-		this.props.events.dragStart(e, blockInfo);
-	},
-	dragOver: function (e) {
-		var blockInfo = {
-			id: this.props.data.id,
-			index: this.props.index,
-			parentSlotId: this.props.parentSlotId
-		}
-		this.props.events.dragOver(e, blockInfo);
-	},
-	render: function () {
-
-		var block = this.props.block;
-		var events = this.props.events;
-
-		var classes = 'block ' + block.type;
-
-		var styles = {
-			height: '100px'
-		}
-
-		var content;
-		if (block.type === 'image') {
-			var s = { backgroundImage: 'url("' + block.imageSrc + '")' };
-			content = (<div style={s}></div>);
-		} else if (block.type === 'text') {
-			content = (<p>{block.text}</p>);
-		} else if (block.type === 'textplusimage') {
-			content = (
-				<div className="row">
-					<div className="image col-xs-6">
-						<img src={block.imageSrc} height="100"/>
-					</div>
-					<div className="text col-xs-6">
-						<p>{block.text}</p>
-					</div>
-				</div>
-			);
-		}
-
-		var nameStyles = {
-			display: 'block',
-			position: 'absolute',
-			top: 0,
-			left: 0,
-			background: 'gray',
-			color: 'white'
-		}
-
-		return (
-			<div
-				className={classes}
-				draggable="true"
-				style={styles}
-				onDragStart={this.dragStart}
-				onDragEnd={events.dragEnd}
-				onDragOver={this.dragOver}
-				>
-					{content}
-					<span style={nameStyles}>{block.name}{block.id}</span>
-			</div>
-		)
-	}
-});
-
-
-
-
-*/
--- src/components/components.js
+++ src/components/components.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 // import { connect } from 'react-redux'
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
 
 import './components.less'
 
 import EditorLeft from './EditorLeft/editorleft'
 import EditorRight from './EditorRight/editorright'
 
-const Components = () => {
-	return (
-		<div>
-			<div className="col-xs-4 pull-left">
-				<EditorLeft />
+@DragDropContext(HTML5Backend)
+export default class Components extends Component {
+	render() {
+		return (
+			<div>
+				<div className="col-xs-4 pull-left">
+					<EditorLeft />
+				</div>
+				<div className="col-xs-8 pull-right">
+					<EditorRight />
+				</div>
 			</div>
-			<div className="col-xs-8 pull-right">
-				<EditorRight />
-			</div>
-		</div>
-	)
+		)
+	}
 }
-
-
-export default Components
