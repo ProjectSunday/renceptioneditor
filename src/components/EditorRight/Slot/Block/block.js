8d01980a55e1e8a0e22c5755369223c624e24a5b
Hai Nguyen <hai.nguyen@salesforce.com>
2016-02-15 21:28:49 -0500

    [Mon Feb 15 21:28:49 EST 2016] droptarget working

--- src/components/EditorRight/Slot/Block/block.js
+++ src/components/EditorRight/Slot/Block/block.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+// import { connect } from 'react-redux'
 import { DragSource, DropTarget } from 'react-dnd'
 
 import { dragBlock } from '../../../../Actions/actions'
@@ -9,7 +9,7 @@ import './block.less'
 const blockSource = {
 	beginDrag(props) {
 
-		console.log('yo', props.block.id)
+		// console.log('yo', props.block.id)
 
 		return {
 			id: props.block.id
@@ -17,40 +17,50 @@ const blockSource = {
 	}
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-	return {
-		onDragStart: () => {
-			dispatch(dragBlock(ownProps.slotId, ownProps.block.id))
-		}
+const blockTarget = {
+	hover(props, monitor, component) {
+
+		if (monitor.getItem().id == props.block.id) { return }
+
+		console.count('hover')
+		console.log(props)
+
 	}
 }
 
+// const mapDispatchToProps = (dispatch, ownProps) => {
+// 	return {
+// 		onDragStart: () => {
+// 			dispatch(dragBlock(ownProps.slotId, ownProps.block.id))
+// 		}
+// 	}
+// }
 
-@connect(undefined, mapDispatchToProps)
+
+// @connect(undefined, mapDispatchToProps)
+@DropTarget('BLOCK', blockTarget, (connect) => ({
+	connectDropTarget: connect.dropTarget()
+}))
 @DragSource('BLOCK', blockSource, (connect, monitor) => ({
 	connectDragSource: connect.dragSource(),
   	isDragging: monitor.isDragging()
 }))
 export default class Block extends Component {
 	render() {
-		const { block, slotId, onDragStart, connectDragSource } = this.props;
+		const { block, slotId, connectDragSource, connectDropTarget, isDragging } = this.props;
 	
-		let styles = { display: 'block' }
+		let styles = { display: 'block', opacity: isDragging ? 0 : 1 }
 
 
+		
 		let blockInnards = <div className="innards" style={ { background: 'blue', height: 50 } }>NNNNNNNNNNNNNNNNNNNNNNNN</div>
 
-		if (block.name == 'dragging') {		
-			blockInnards = <p className="innards" style={ { height: 75 } }>draggingdraggingdraggingdraggingdraggingdraggingdragging</p>
-		}
-
-
-		return connectDragSource(
+		return connectDragSource(connectDropTarget(
 			<div className="block" style={styles}>
 				<span className="name">{block.name}{block.id}</span>
 				{blockInnards}
 			</div>
-		)
+		))
 	}
 
 }
