505fc9a1659b4323691823c98681b4d17a6e662e
Hai Nguyen <hai.nguyen@salesforce.com>
2016-02-21 00:32:15 -0500

    [Sun Feb 21 00:32:15 EST 2016]

--- src/components/EditorRight/Slot/slot.js
+++ src/components/EditorRight/Slot/slot.js
@@ -40,28 +40,16 @@ export default class Slot extends React.Component {
 	render() {
 		const { slot, index, dropzone, blocks, onClick, moveBlock, insertDropZone } = this.props
 
-		// console.group('slot')
-		// console.count()
-		// console.log('blocks ', blocks[0].id, blocks[1].id, blocks[2].id)
-		// console.log('dropzone', dropzone)
-
-
-		// let hasDropZone = (index === dropzone.slotIndex || index === dropzone.previous.slotIndex)
-
-		let hasDropZoneAppearing 	= index === dropzone.slotIndex
-		let hasDropZoneDisappearing = index === dropzone.previous.slotIndex
+		let hasDropZone = index === dropzone.slotIndex
 
 		let blockNodes = []
 		blocks.forEach((b, i) => {
-			if (hasDropZoneAppearing && i === dropzone.blockIndex) {
-				blockNodes.push(<DropZone key="dropzone-appearing" isAppearing={true} />)
-			} else if (hasDropZoneDisappearing && i === dropzone.previous.blockIndex) {
-				blockNodes.push(<DropZone key="dropzone-disappearing" isAppearing={false} />)
+			if (hasDropZone && i === dropzone.blockIndex) {
+				blockNodes.push(<DropZone key={'dropzone' + i} appearing={true} />)
 			}
 			blockNodes.push(<Block key={i} index={i} block={b} moveBlock={moveBlock} insertDropZone={insertDropZone}/>)
 		})
 
-		// console.groupEnd()
 		return (
 			<div>
 			<button onClick={onClick}>Add Block</button>
@@ -72,13 +60,3 @@ export default class Slot extends React.Component {
 		)
 	}
 }
-
-
-			// {blocks.map((b, i) => {
-			// 		if (hasDropZone && i == dropzone.blockIndex) {
-			// 			<div key="dropzone">dropzone</div>
-			// 		}
-			// 		<div>huh</div>
-			// 		// <Block key={i} index={i} block={b} moveBlock={moveBlock} insertDropZone={insertDropZone}/>
-			// 	})}
-
