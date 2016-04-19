ee425ea2e52456511f38f14c2604bc3802244529
Hai Nguyen <hai.nguyen@salesforce.com>
2016-04-19 09:49:00 -0400

    [Tue Apr 19 09:49:00 EDT 2016] add in debug

--- src/debug.js
+++ src/debug.js
@@ -8,12 +8,28 @@ window.trace = function () {
     console.log.apply(console, args);
 }
 
-window.red = function () {
+window.r = window.red = function () {
     var caller = ((new Error).stack.split('\n')[2]).trim();
+    caller = caller.split('/')
+    caller = caller[caller.length - 1]
     var args = Array.prototype.slice.call(arguments);
     args.unshift('background: red; color: white;');
     args.unshift('%c' + caller);
     console.log.apply(console, args);
 }
+
+// window.r = function () {
+// 	var caller = ((new Error).stack.split('\n')[2]).trim();
+// 	var args = Array.prototype.slice.call(arguments);
+// 	args.unshift('background: red; color: white;');
+// 	args.unshift('%c' + caller);
+// 	console.log.apply(console, args);
+// }
+
+window.c = function () {
+    console.count.call(console, ':=============================')
+}
+
+window.l = console.log.bind(console);
 
 export default {}
